13668f1db1662760ee95a738586dfe926d2d62b1
rfrcanesin <ricardo@0xcarbon.org>
2023-06-28 11:26:53 +0200

    Update: large numbers work now. The curve secp256k1 is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
-use modular::*;
-use modinverse::*;
-use rand::Rng;
+use curv::{BigInt, arithmetic::{Modulo, Samplable, traits::Integer, Converter}};
+use lazy_static::lazy_static;
 
-pub const CURVE_A: i32 = 2;
-pub const CURVE_B: i32 = 2;
-pub const CURVE_CHAR: u32 = 17;
+lazy_static! {
 
-pub const GEN_X: i32 = 5;
-pub const GEN_Y: i32 = 1;
-pub const GEN_ORDER: u32 = 19;
+    static ref CURVE_A: BigInt = BigInt::from(0);
+    static ref CURVE_B: BigInt = BigInt::from(7);
+    static ref CURVE_CHAR: BigInt = BigInt::from_hex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f").unwrap();
+
+    static ref GEN_X: BigInt = BigInt::from_hex("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798").unwrap();
+    static ref GEN_Y: BigInt = BigInt::from_hex("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8").unwrap();
+    static ref GEN_ORDER: BigInt = BigInt::from_hex("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141").unwrap();
+    
+}
+
+
 
 #[derive(Debug,Clone)]
 enum Point{
-    Affine(Modulo,Modulo),
+    Affine(BigInt,BigInt),
     AtInfinity,
 }
 
@@ -22,13 +27,18 @@ pub struct PointElpCurve {
 }
 
 impl PointElpCurve {
-    pub fn new_affine(x: i32, y: i32) -> PointElpCurve {
-        if !(0.is_congruent(y*y-x*x*x-CURVE_A*x-CURVE_B, CURVE_CHAR)) {
+    pub fn new_affine(x: &BigInt, y: &BigInt) -> PointElpCurve {
+        
+        let y_squared = BigInt::mod_mul(&y, &y, &CURVE_CHAR);
+        let x_cubed = BigInt::mod_mul(&BigInt::mod_mul(&x, &x, &CURVE_CHAR), &x, &CURVE_CHAR); 
+        let a_times_x = BigInt::mod_mul(&CURVE_A, &x, &CURVE_CHAR);
+        let expression_elliptic_curve = BigInt::mod_sub(&BigInt::mod_sub(&BigInt::mod_sub(&y_squared, &x_cubed, &CURVE_CHAR), &a_times_x, &CURVE_CHAR), &CURVE_B, &CURVE_CHAR);
+        if expression_elliptic_curve != BigInt::from(0) { 
             panic!("This is not a point on the curve!");
         }
 
-        let x = x.to_modulo(CURVE_CHAR);
-        let y = y.to_modulo(CURVE_CHAR);
+        let x = x.modulus(&CURVE_CHAR);
+        let y = y.modulus(&CURVE_CHAR);
 
         let point = Point::Affine(x,y);
 
@@ -41,40 +51,40 @@ impl PointElpCurve {
     }
 
     pub fn generator() -> PointElpCurve {
-        PointElpCurve { point: Point::Affine(GEN_X.to_modulo(CURVE_CHAR), GEN_Y.to_modulo(CURVE_CHAR))}
+        PointElpCurve { point: Point::Affine(GEN_X.modulus(&CURVE_CHAR), GEN_Y.modulus(&CURVE_CHAR))}
     }
 
     pub fn double(&self) -> PointElpCurve {
-        match self.point {
+        match &self.point {
            Point::AtInfinity => PointElpCurve::new_atinfinity(),
            Point::Affine(x, y) => {
-            if y == 0.to_modulo(CURVE_CHAR) {
+            if y == &BigInt::from(0) {
                 PointElpCurve::new_atinfinity()
             } else {
-                let numerator = 3.to_modulo(CURVE_CHAR)*x*x + CURVE_A.to_modulo(CURVE_CHAR);
-                let denominator = 2.to_modulo(CURVE_CHAR) * y ;
-                let try_inverse_denominator = modinverse(denominator.remainder(), CURVE_CHAR as i32);
-                let inverse_denominator: Modulo;
+                let numerator = BigInt::mod_add(&BigInt::mod_mul(&BigInt::mod_mul(&BigInt::from(3),x, &CURVE_CHAR), x, &CURVE_CHAR), &CURVE_A, &CURVE_CHAR);
+                let denominator = BigInt::mod_mul(&BigInt::from(2), y, &CURVE_CHAR);
+                let try_inverse_denominator = BigInt::mod_inv(&denominator, &CURVE_CHAR);
+                let inverse_denominator: BigInt;
                 match try_inverse_denominator {
-                    Some(inv) => { inverse_denominator = inv.to_modulo(CURVE_CHAR); },
+                    Some(inv) => { inverse_denominator = inv; },
                     None => { panic!("Improbable error! Could not invert denominator in double()"); },
                 }
-                let slope_tangent = numerator * inverse_denominator;
+                let slope_tangent = BigInt::mod_mul(&numerator, &inverse_denominator, &CURVE_CHAR);
 
-                let new_x = slope_tangent*slope_tangent - 2.to_modulo(CURVE_CHAR)*x;
-                let new_y = slope_tangent*(x - new_x) - y;
+                let new_x = BigInt::mod_sub(&BigInt::mod_mul(&slope_tangent, &slope_tangent, &CURVE_CHAR), &BigInt::mod_mul(&BigInt::from(2), x, &CURVE_CHAR), &CURVE_CHAR);
+                let new_y = BigInt::mod_sub(&BigInt::mod_mul(&slope_tangent, &BigInt::mod_sub(x, &new_x, &CURVE_CHAR), &CURVE_CHAR), y, &CURVE_CHAR);
 
-                PointElpCurve::new_affine(new_x.remainder(), new_y.remainder())
+                PointElpCurve::new_affine(&new_x, &new_y)
             }
            }
         }
     }
 
     pub fn plus(&self, other: &PointElpCurve) -> PointElpCurve {
-        match self.point {
+        match &self.point {
             Point::AtInfinity => other.clone(),
             Point::Affine(x1, y1) => {
-                match other.point {
+                match &other.point {
                     Point::AtInfinity => self.clone(),
                     Point::Affine(x2, y2) => {
                         if x1 == x2 {
@@ -85,20 +95,20 @@ impl PointElpCurve {
                                 PointElpCurve::new_atinfinity()
                             }
                         } else {
-                            let numerator = y2 - y1;
-                            let denominator = x2 - x1 ;
-                            let try_inverse_denominator = modinverse(denominator.remainder(), CURVE_CHAR as i32);
-                            let inverse_denominator: Modulo;
+                            let numerator = BigInt::mod_sub(y2, y1, &CURVE_CHAR);
+                            let denominator = BigInt::mod_sub(x2, x1, &CURVE_CHAR);
+                            let try_inverse_denominator = BigInt::mod_inv(&denominator, &CURVE_CHAR);
+                            let inverse_denominator: BigInt;
                             match try_inverse_denominator {
-                                Some(inv) => { inverse_denominator = inv.to_modulo(CURVE_CHAR); },
+                                Some(inv) => { inverse_denominator = inv; },
                                 None => { panic!("Improbable error! Could not invert denominator in plus()"); },
                             }
-                            let slope_chord = numerator * inverse_denominator;
+                            let slope_chord = BigInt::mod_mul(&numerator, &inverse_denominator, &CURVE_CHAR);
 
-                            let new_x = slope_chord*slope_chord - x1 - x2;
-                            let new_y = slope_chord*(x1 - new_x) - y1;
+                            let new_x = BigInt::mod_sub(&BigInt::mod_sub(&BigInt::mod_mul(&slope_chord, &slope_chord, &CURVE_CHAR), x1, &CURVE_CHAR), x2, &CURVE_CHAR);
+                            let new_y = BigInt::mod_sub(&BigInt::mod_mul(&slope_chord, &BigInt::mod_sub(x1, &new_x, &CURVE_CHAR), &CURVE_CHAR), y1, &CURVE_CHAR);
 
-                            PointElpCurve::new_affine(new_x.remainder(), new_y.remainder())
+                            PointElpCurve::new_affine(&new_x, &new_y)
                         }
                     }
                 }
@@ -106,16 +116,21 @@ impl PointElpCurve {
         }
     }
 
-    pub fn multiple(&self, times: u32) -> PointElpCurve {
+    pub fn multiple(&self, times: &BigInt) -> PointElpCurve {
+
+        if times < &BigInt::from(0) {
+            panic!("Negative multiples not implemented!");
+        }
+
         let mut final_point = PointElpCurve::new_atinfinity();
         let mut base = self.clone();
-        let mut count = times;
-        while count > 0 {
-            if count % 2 == 1 {
-                count = count - 1;
+        let mut count = times.clone();
+        while count > BigInt::from(0) {
+            if BigInt::is_odd(&count) {
+                count = count - BigInt::from(1);
                 final_point = final_point.plus(&base);
             }
-            count = count / 2;
+            count = count / BigInt::from(2);
             base = base.double();
         }
         final_point
@@ -123,42 +138,42 @@ impl PointElpCurve {
 
 }
 
-pub fn generate_private_key() -> u32 {
-    rand::thread_rng().gen_range(1..GEN_ORDER)
+pub fn generate_private_key() -> BigInt {
+    BigInt::strict_sample_range(&BigInt::from(0), &GEN_ORDER)
 }
 
-pub fn generate_public_key(priv_key: u32) -> PointElpCurve {
+pub fn generate_public_key(priv_key: &BigInt) -> PointElpCurve {
     let gen = PointElpCurve::generator();
     gen.multiple(priv_key)
 }
 
-pub fn generate_signature(hash_msg: i32, priv_key: u32) -> (i32, i32) {
+pub fn generate_signature(hash_msg: &BigInt, priv_key: &BigInt) -> (BigInt, BigInt) {
     
-    let mut r: i32;
-    let mut s: i32;
+    let mut r: BigInt;
+    let mut s: BigInt;
 
     loop {
 
-    let ephem_key = rand::thread_rng().gen_range(1..GEN_ORDER);
+    let ephem_key = BigInt::strict_sample_range(&BigInt::from(0), &GEN_ORDER);
     let gen = PointElpCurve::generator();
     
-    match gen.multiple(ephem_key).point {
-        Point::Affine(x,_) => { r = x.remainder(); },
+    match gen.multiple(&ephem_key).point {
+        Point::Affine(x,_) => { r = x; },
         Point::AtInfinity => { panic!("Improbable error! Ephemeral key was the order of the generator"); },
     }
 
-    if r == 0 { continue; }
+    if r == BigInt::from(0) { continue; }
     
-    let try_inverse_ephem_key = modinverse(ephem_key as i32, GEN_ORDER as i32);
-    let inverse_ephem_key: Modulo;
+    let try_inverse_ephem_key = BigInt::mod_inv(&ephem_key, &GEN_ORDER);
+    let inverse_ephem_key: BigInt;
     match try_inverse_ephem_key {
-        Some(inv) => { inverse_ephem_key = inv.to_modulo(GEN_ORDER); },
+        Some(inv) => { inverse_ephem_key = inv; },
         None => { panic!("Improbable error! Could not invert ephemeral key"); },
     }
 
-    s = ((hash_msg.to_modulo(GEN_ORDER) + ((priv_key as i32).to_modulo(GEN_ORDER) * r.to_modulo(GEN_ORDER))) * inverse_ephem_key).remainder();
+    s = BigInt::mod_mul(&BigInt::mod_add(hash_msg, &BigInt::mod_mul(priv_key, &r, &GEN_ORDER), &GEN_ORDER), &inverse_ephem_key, &GEN_ORDER);
     
-    if s == 0 { continue; }
+    if s == BigInt::from(0) { continue; }
 
     break;
     }   
@@ -166,29 +181,29 @@ pub fn generate_signature(hash_msg: i32, priv_key: u32) -> (i32, i32) {
     (r,s)
 }
 
-pub fn verify_signature(hash_msg: i32, pub_key: &PointElpCurve, r: i32, s: i32) -> bool {
+pub fn verify_signature(hash_msg: &BigInt, pub_key: &PointElpCurve, r: &BigInt, s: &BigInt) -> bool {
 
-    if !(0 < r && r < GEN_ORDER as i32 && 0 < s && s < GEN_ORDER as i32){
+    if !(&BigInt::from(0) < r && r < &GEN_ORDER && &BigInt::from(0) < s && s < &GEN_ORDER){
         return false;
     }
 
-    let try_inverse_s = modinverse(s, GEN_ORDER as i32);
-    let inverse_s: Modulo;
+    let try_inverse_s = BigInt::mod_inv(s, &GEN_ORDER);
+    let inverse_s: BigInt;
     match try_inverse_s {
-        Some(inv) => { inverse_s = inv.to_modulo(GEN_ORDER); },
+        Some(inv) => { inverse_s = inv; },
         None => { panic!("Improbable error! Could not invert s while verifying signature"); },
     }
 
-    let u1 = inverse_s * hash_msg.to_modulo(GEN_ORDER);
-    let u2 = inverse_s * r.to_modulo(GEN_ORDER);
+    let u1 = BigInt::mod_mul(&inverse_s, hash_msg, &GEN_ORDER);
+    let u2 = BigInt::mod_mul(&inverse_s, r, &GEN_ORDER);
 
     let gen = PointElpCurve::generator();
-    let point1 = gen.multiple(u1.remainder() as u32);
-    let point2 = pub_key.multiple(u2.remainder() as u32);
+    let point1 = gen.multiple(&u1);
+    let point2 = pub_key.multiple(&u2);
     let point_to_check = point1.plus(&point2);
 
     match point_to_check.point {
         Point::AtInfinity => { false },
-        Point::Affine(x,_) => { x.remainder().is_congruent(r, GEN_ORDER) },
+        Point::Affine(x,_) => { x.modulus(&GEN_ORDER) == r.modulus(&GEN_ORDER) },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-use ecdsa::{self, GEN_ORDER};
-use rand::Rng;
+use ecdsa;
+use curv::{BigInt, arithmetic::Converter};
 
-//a = 2, b = 2, p = 17, gen_x = 5, gen_y = 1, gen_order = 19
+//Implemented curve: secp256k1  (you can enter manually other curves in lib.rs)
 
 fn main() { 
 
-    let priv_key = ecdsa::generate_private_key();
-    let pub_key = ecdsa::generate_public_key(priv_key);
+    //let priv_key = ecdsa::generate_private_key();
+    let priv_key = BigInt::from_hex("bb307211567f2cb78c58fe9c089a30f9b5868334ee1015c52d74b57594b6b831").unwrap();
+    let pub_key = ecdsa::generate_public_key(&priv_key);
 
-    let hash_msg = rand::thread_rng().gen_range(1..=GEN_ORDER) as i32;
-    let sig = ecdsa::generate_signature(hash_msg as i32, priv_key);
-    let (r,s) = sig;
+    let hash_msg = BigInt::from_hex("ac4e151e612b0669c89b4bead166a7e788a22d7edef8070fa469eb2194e49f14").unwrap();
+    let sig = ecdsa::generate_signature(&hash_msg, &priv_key);
+    let (r,s) = &sig;
 
-    let verify = ecdsa::verify_signature(hash_msg as i32, &pub_key, r, s);
+    let verify = ecdsa::verify_signature(&hash_msg, &pub_key, r, s);
 
-    let rand_r = rand::thread_rng().gen_range(1..GEN_ORDER) as i32;
-    let rand_s = rand::thread_rng().gen_range(1..GEN_ORDER) as i32;
-    let rand_verify = ecdsa::verify_signature(hash_msg as i32, &pub_key, rand_r, rand_s);
+    //If you want to verify the public key and other signatures, use this website: https://paulmillr.com/noble/
+    //For a random signature, in the section "Signature", copy the number called "compact" and split it in half in the strings below
+
+    let sig_web = (BigInt::from_hex("bb7c12dc3a46de64fb06d73314695570b90095fc99dc95f96ac35214bd36446e").unwrap(), BigInt::from_hex("6609c43b2c60fbcc984b1a7dcb1616b51788aab402416e042c0299fa6dc2dc16").unwrap());
+    let (r_web,s_web) = &sig_web;
+    let verify_web = ecdsa::verify_signature(&hash_msg, &pub_key, r_web, s_web);
 
     println!("Private key: {priv_key}");
     println!("Public key: {:?}", pub_key);
     println!("Hashed message: {hash_msg}");
     println!("Signature: {:?}", sig);
     println!("Verification: {verify}");
-    println!("Random signature: {:?}", (rand_r,rand_s));
-    println!("Verification: {rand_verify}");
+    println!("Signature from website: {:?}", sig_web);
+    println!("Verification: {verify_web}");
 
 }
